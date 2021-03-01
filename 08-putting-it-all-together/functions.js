1fbd4288a634ff54af69ab237b141a1c7beb9b67
ValeriiaShur <valeriia.shurupina@yahoo.com>
2021-03-01 13:44:32 +0100

    Building a dashboard

---
+++ 08-putting-it-all-together/functions.js
@@ -0,0 +1,10 @@
+"use strict";
+function formatSales(d) {
+  var prefix = d3.formatPrefix(d);
+  var num = prefix.scale(d).toFixed();
+  return num + prefix.symbol;
+}
+
+function compareNums(a, b) {
+  return a - b;
+}
